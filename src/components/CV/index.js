4a8f38fde2bdbfa81b60756736bbf892f0b3c4f5
rafaguerrero <rafa.guerrero@shellock.co>
2022-12-05 14:57:36 +0100

    First

---
+++ src/components/CV/index.js
@@ -0,0 +1,16 @@
+import Entry from '../../components/Entry';
+import './styles.scss';
+
+function CV() {
+  return (
+    <div className="cv">
+      <h1>hola</h1>
+
+      <Entry/>
+      <Entry/>
+      <Entry/>
+    </div>
+  );
+}
+
+export default CV;
---
+++ src/components/Entry/index.js
@@ -0,0 +1,11 @@
+import './styles.scss';
+
+function Entry() {
+  return (
+    <div className="entry">
+      <h2>Entry</h2>
+    </div>
+  );
+}
+
+export default Entry;
